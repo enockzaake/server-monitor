c425095b77089bf9ed6f3b061d3923257f022d6e
enockzaake <enockzaake@gmail.com>
2024-02-12 15:29:09 +0530

    frontend docker config

--- frontend/components/test.tsx
+++ frontend/components/test.tsx
@@ -8,13 +8,15 @@ function MyComponent() {
   const divRef = useRef(null);
   const [textColor, setTextColor] = useState("red");
 
+  const WS_URL = process.env.BASE_WS_URL as string;
+
   const handleSocket = useCallback(() => {
     const isBrowser = typeof window !== "undefined";
     if (!isBrowser) return;
 
     // Create new WebSocket instance if not already created
     if (!wsRef.current || wsRef.current.readyState !== WebSocket.OPEN) {
-      const ws = new WebSocket("ws://localhost:2000/ws");
+      const ws = new WebSocket(`${WS_URL}/ws`);
       wsRef.current = ws;
 
       ws.onopen = () => {
@@ -51,10 +53,13 @@ function MyComponent() {
 
   useEffect(() => {
     if (divRef.current) {
-      const paragraph = document.createElement("p");
+      const paragraph: HTMLElement = document.createElement("p");
       paragraph.style.color = textColor;
       paragraph.textContent = timestamp;
-      divRef.current.insertBefore(paragraph, divRef.current.firstChild);
+      (divRef.current as HTMLElement).insertBefore(
+        paragraph,
+        (divRef.current as HTMLElement).firstChild
+      );
       setTextColor((prevColor: string) =>
         prevColor === "red" ? "green" : "red"
       );
